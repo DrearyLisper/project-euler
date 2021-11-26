f11d8532776aa121491052bd34ae0d1e3db9d416
Dreary Lisper <drearylisper@gmail.com>
2021-11-26 20:13:36 +0000

    Problem 002.

--- main.hs
+++ main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-p01 :: Int
-p01 = sum $ filter (\x -> any ((== 0).mod x) [3,5]) [0..999]
+p001 :: Int
+p001 = sum $ filter (\x -> any ((== 0).mod x) [3,5]) [0..999]
+
+p002 :: Int
+p002 = sum $ filter even (takeWhile (<4000000) fibs)
+  where
+    fibs =  0 : 1 : zipWith (+) fibs (tail fibs)
 
 main = print "Hello, world!"
